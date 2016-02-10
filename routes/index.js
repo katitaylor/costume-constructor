4dd611804ce48c63afb02cdef7c976b564c0094d
kati taylor <katherine.pelletier@gmail.com>
2016-02-10 10:32:07 -0600

    ERR_CONNECTION_REFUSED haunts me again

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Costume Construction Zone' });
+  res.render('index', { title: 'Costume Construction Zone!' });
 });
 
-module.exports = router;
+/* POST costume. */
+router.post('/', function(req, res, next) {
+  res.render('costumes', { theme: wearer: });
+});
+
